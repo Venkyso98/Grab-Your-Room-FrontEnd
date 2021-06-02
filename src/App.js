93449815493a4d8db34ead16ad9685cb1e826297
Venktesh Soma <venkteshs@cybage.com>
2021-06-02 20:00:01 +0530

    Implemendt the route of the all rooms

--- src/App.js
+++ src/App.js
@@ -3,30 +3,21 @@ import Home from "./components/Home/Home";
 import SearchRoom from "./components/Search/SearchRoom";
 import About from "./components/About.jsx";
 import { Route, Switch } from "react-router";
-import { useState } from "react";
+import AllRooms from "./components/AllRooms/AllRooms";
 
 function App() {
-  const [data, setData] = useState("");
-
-  const handleOnClick = (dataValue) => {
-    console.log("In handle On Click:",dataValue);
-    setData(dataValue);
-  }
   return (
     <div>
       <Switch>
         <Route path="/" component={Home} exact />
-        {/* <Route path="/search" component={SearchRoom} exact /> */}
-        {/* <Route path="/about" exact component={About}  /> */}
+        <Route path="/search" component={SearchRoom} exact />
 
         {/* custom props in route */}
-        <Route path="/search" render={(props) => (<SearchRoom onClickData={handleOnClick} {...props} />)} exact />
+        {/* <Route path="/search" render={(props) => (<SearchRoom onClickData={handleOnClick} {...props} />)} exact /> */}
         {/* <Route path="/about" render={(props) => (<About onClickResponseData={data} {...props} />)} exact /> */}
-        <Route path="/about/:value" component={About} exact />
-    
+        <Route path="/allrooms" component={AllRooms} exact />
+        <Route path="/about" component={About} exact />
       </Switch>
-      {/* <Home /> */}
-      {/* <SearchRoom /> */}
     </div>
   );
 }
