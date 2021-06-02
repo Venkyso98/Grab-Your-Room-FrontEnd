3fb73b791f0fe63e7bca20dba6f7008f628973da
Venktesh Soma <venkteshs@cybage.com>
2021-06-02 11:09:19 +0530

    Implemented the custom props for the route one

--- src/App.js
+++ src/App.js
@@ -1,16 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
-import Home from './components/Home/Home';
-import SearchRoom from './components/Search/SearchRoom';
-import About from "./components/About.jsx"
-import { Route, Switch } from 'react-router';
+import "./App.css";
+import Home from "./components/Home/Home";
+import SearchRoom from "./components/Search/SearchRoom";
+import About from "./components/About.jsx";
+import { Route, Switch } from "react-router";
+import { useState } from "react";
 
 function App() {
+  const [data, setData] = useState("");
+
+  const handleOnClick = (dataValue) => {
+    console.log("In handle On Click:",dataValue);
+    setData(dataValue);
+  }
   return (
     <div>
       <Switch>
         <Route path="/" component={Home} exact />
-        <Route path="/search" component={SearchRoom} exact />
+        {/* <Route path="/search" component={SearchRoom} exact /> */}
+        {/* <Route path="/about" exact component={About}  /> */}
+
+        {/* custom props in route */}
+        <Route path="/search" render={(props) => (<SearchRoom onClickData={handleOnClick} {...props} />)} exact />
+        {/* <Route path="/about" render={(props) => (<About onClickResponseData={data} {...props} />)} exact /> */}
+        <Route path="/about/:value" component={About} exact />
+    
       </Switch>
       {/* <Home /> */}
       {/* <SearchRoom /> */}
