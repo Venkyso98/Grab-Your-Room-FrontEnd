955b2d87b516f0a5cdf013e94574cae183d97864
Venktesh Soma <venkteshs@cybage.com>
2021-06-02 11:06:37 +0530

    Raw implementation of al rooms page done via passing checkin in url

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,11 +1,33 @@
-import React from 'react'
+import axios from "axios";
+import React, { useEffect, useState } from "react";
 
-function About() {
-	return (
-		<div>
-			<h1>About us</h1>
-		</div>
-	)
+function About(props) {
+  console.log("PRops:", props);
+  const [dummy, setDummy] = useState([]);
+  console.log("Dummy:", props.match.params.value);
+
+  useEffect(() => {
+    axios
+      .get(`http://localhost:5000/rooms/getAllRooms/${props.match.params.value}`)
+      .then((response) => {
+        console.log("Response Get in About:", response.data);
+		setDummy(response.data)
+        // setFormResponse([...response.data]);
+        // props.onClickData(response.data);
+      })
+      .catch((err) => {
+        console.log("Error:", err);
+      });
+  }, []);
+
+  return (
+    <div>
+      <h1>About us</h1>
+	  {dummy.map((getData) => (
+		  console.log("GetData:",getData)
+	  ))}
+    </div>
+  );
 }
 
-export default About
+export default About;
