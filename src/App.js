6d7d401eb4ebd26946f904c95f044f9e2234e8f8
Venktesh Soma <venkteshs@cybage.com>
2021-06-04 15:20:08 +0530

    implemented the route for the room receipt

--- src/App.js
+++ src/App.js
@@ -3,8 +3,9 @@ import Home from "./components/Home/Home";
 import SearchRoom from "./components/Search/SearchRoom";
 import About from "./components/About.jsx";
 import { Route, Switch } from "react-router";
-import AllRooms from "./components/AllRooms/AllRooms";
-import RoomDetails from "./components/RoomDetails/RoomDetails";
+import AllRooms from "./components/Rooms/AllRooms/AllRooms";
+import RoomDetails from "./components/Rooms/RoomDetails/RoomDetails";
+import RoomReceipt from "./components/Rooms/Receipt/RoomReceipt";
 
 function App() {
   return (
@@ -16,9 +17,10 @@ function App() {
         {/* custom props in route */}
         {/* <Route path="/search" render={(props) => (<SearchRoom onClickData={handleOnClick} {...props} />)} exact /> */}
         {/* <Route path="/about" render={(props) => (<About onClickResponseData={data} {...props} />)} exact /> */}
-        <Route path="/allrooms" component={AllRooms} exact />
+        <Route path="/rooms-listing" component={AllRooms} exact />
         <Route path="/about" component={About} exact />
-        <Route path="/room-details/:id" component={RoomDetails}></Route>
+        <Route path="/room-details/:id" component={RoomDetails} exact></Route>
+        <Route path="/room-receipt" component={RoomReceipt} exact></Route>
       </Switch>
     </div>
   );
