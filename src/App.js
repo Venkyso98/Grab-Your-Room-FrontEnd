6e9982196385eeb64b67ff5f1398b1af2303f934
Venktesh Soma <venkteshs@cybage.com>
2021-06-05 19:08:27 +0530

    Tried to implement the cssbaseline concept

--- src/App.js
+++ src/App.js
@@ -6,22 +6,25 @@ import { Route, Switch } from "react-router";
 import AllRooms from "./components/Rooms/AllRooms/AllRooms";
 import RoomDetails from "./components/Rooms/RoomDetails/RoomDetails";
 import RoomReceipt from "./components/Rooms/Receipt/RoomReceipt";
+import CssBaseline from '@material-ui/core/CssBaseline';
 
 function App() {
   return (
     <div>
+      {/* <Demo /> */}
       <Switch>
         <Route path="/" component={Home} exact />
         <Route path="/search-room" component={SearchRoom} exact />
 
         {/* custom props in route */}
-        {/* <Route path="/search" render={(props) => (<SearchRoom onClickData={handleOnClick} {...props} />)} exact /> */}
+        {/* <Route path="/search" render={(props) => (<SearchRoom onClickData={handleOnClick} {...props} />)} exact />
         {/* <Route path="/about" render={(props) => (<About onClickResponseData={data} {...props} />)} exact /> */}
         <Route path="/rooms-listing" component={AllRooms} exact />
         <Route path="/about" component={About} exact />
         <Route path="/room-details/:id" component={RoomDetails} exact></Route>
         <Route path="/room-receipt" component={RoomReceipt} exact></Route>
       </Switch>
+      {/* <CssBaseline /> */}
     </div>
   );
 }
