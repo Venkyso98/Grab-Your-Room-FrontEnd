e4682f139612776dac92f4b66c96d4ff72297f77
Venktesh Soma <venkteshs@cybage.com>
2021-06-07 16:50:13 +0530

    Done the drawer and breakpoints implementation

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,46 +1,134 @@
-import { Box, Button } from "@material-ui/core";
-import React from "react";
+import {
+  Box,
+  Button,
+  Drawer,
+  Hidden,
+  IconButton,
+  Typography,
+} from "@material-ui/core";
+import React, { useState } from "react";
 import { useStyles } from "./Home.style";
 import ArrowRightAltIcon from "@material-ui/icons/ArrowRightAlt";
 import { Link } from "react-router-dom";
+import MenuIcon from "@material-ui/icons/Menu";
+import CloseIcon from "@material-ui/icons/Close";
 
 function Home() {
   const classes = useStyles();
+  const [drawer, setDrawer] = useState(false);
+
+  const handleDrawerToggle = () => {
+    setDrawer(!drawer);
+  };
+
   return (
-    <nav className={classes.navBar}>
-      <div className={classes.mainContent}>
-        <div className={classes.leftPane}>
-          <Link to="/">Home</Link>
-          <Link to="/search-room">Rooms</Link>
-          <a>Amenities</a>
-        </div>
-        <div className={classes.logoDiv}>
-          <a href="">Grab Your Room</a>
-        </div>
-        <div className={classes.rightPane}>
-          <a href="">Gallery</a>
-          <a href="">Blog</a>
-          <a href="">Contact</a>
-        </div>
+    <React.Fragment>
+      <div className={classes.homeImage}>
+        <nav>
+          <div className={classes.mainContent}>
+            <Hidden mdUp>
+              <MenuIcon
+                onClick={handleDrawerToggle}
+                className={classes.menuIcon}
+              />
+            </Hidden>
+            <Hidden mdDown>
+              <div className={classes.leftPane}>
+                <Link to="/">Home</Link>
+                <Link to="/search-room">Rooms</Link>
+                <a>Amenities</a>
+              </div>
+            </Hidden>
+            <div className={classes.logoDiv}>
+              <a href="">Grab Your Room</a>
+            </div>
+            <Hidden mdDown>
+              <div className={classes.rightPane}>
+                <a href="">Gallery</a>
+                <a href="">Blog</a>
+                <a href="">Contact</a>
+              </div>
+            </Hidden>
+          </div>
+        </nav>
+      </div>
+      <div className="boxContentDiv">
+        <Box className={classes.BoxContent}>
+          <Typography variant="h1" className={classes.boxContentText}>
+            Luxury
+          </Typography>
+          <Typography variant="h4" className={classes.boxContentText}>
+            Hotels & Resort
+          </Typography>
+
+          <Link to="/search-room" className={classes.bookNowContent}>
+            <Button
+              variant="contained"
+              color="primary"
+              className={classes.bookNow}
+              endIcon={<ArrowRightAltIcon></ArrowRightAltIcon>}
+            >
+              Book Now
+            </Button>
+          </Link>
+        </Box>
       </div>
 
-      <Box className={classes.BoxContent}>
-        <p>Luxury</p>
-        <p>Hotels & Resort</p>
-        {/* <div className={classes.bookNow}>Book Now</div> */}
-
-        <Link to="/search-room" className={classes.bookNowContent}>
-          <Button
-            variant="contained"
-            color="primary"
-            className={classes.bookNow}
-            endIcon={<ArrowRightAltIcon></ArrowRightAltIcon>}
+      {/* Drawer */}
+      <div>
+        <Drawer
+          open={drawer}
+          onClose={handleDrawerToggle}
+          anchor="left"
+          // transitionDuration={{ enter: 1000, exit: 30000 }}
+          classes={{
+            paper: classes.drawerPaper,
+          }}
+          ModalProps={{
+            keepMounted: true, // Better open performance on mobile.
+          }}
+        >
+          <IconButton
+            onClick={handleDrawerToggle}
+            className={classes.closeMenuButton}
           >
-            Book Now
-          </Button>
-        </Link>
-      </Box>
-    </nav>
+            <CloseIcon />
+          </IconButton>
+          <div className={classes.mainDrawerContent}>
+            <div>
+              <Link to="/" className={classes.navLinks}>
+                Home
+              </Link>
+            </div>
+            <div>
+              <Link to="/rooms-listing" className={classes.navLinks}>
+                Rooms
+              </Link>
+            </div>
+            <div>
+              <Link to="#" className={classes.navLinks}>
+                Amenities
+              </Link>
+            </div>
+            <div>
+              <Link to="#" className={classes.navLinks}>
+                Gallery
+              </Link>
+            </div>
+            <div>
+              <Link to="#" className={classes.navLinks}>
+                Blog
+              </Link>
+            </div>
+            <div>
+              <Link to="#" className={classes.navLinks}>
+                Contact
+              </Link>
+            </div>
+          </div>
+        </Drawer>
+      </div>
+    </React.Fragment>
   );
 }
 
