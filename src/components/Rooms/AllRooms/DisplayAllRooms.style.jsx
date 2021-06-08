0e0eab06a4bd9f6469bdcead0bcfc0c58c229c41
Venktesh Soma <venkteshs@cybage.com>
2021-06-08 12:58:26 +0530

    separate style file for display rooms

---
+++ src/components/Rooms/AllRooms/DisplayAllRooms.style.jsx
@@ -0,0 +1,42 @@
+import { makeStyles } from "@material-ui/core";
+
+export const useStyles = makeStyles((theme) => ({
+  root: {
+    marginTop: "5rem",
+  },
+  card: {
+    height: "16.5rem",
+    maxWidth: 300,
+
+    "& .hidden-button": {
+      display: "none",
+    },
+    "&:hover .hidden-button": {
+      position: "absolute",
+      display: "block",
+      // backgroundColor: "rgba(100,149,237,0.5)",
+      backgroundColor: "rgba(0,0,0,0.4)",
+      color: "white",
+      fontWeight: "600",
+      padding: "10px",
+      borderRadius: "5px",
+      margin: "-7rem 0 0 7rem",
+      fontFamily: "Play, sans-serif",
+    },
+    "&:hover .roomName": {
+      backgroundColor: "rgba(0,0,0,0.5)",
+      height: "100px",
+      color: "white",
+    },
+  },
+  roomPrice: {
+    textAlign: "center",
+    fontSize: "20px",
+    marginTop: "1rem",
+  },
+  roomCategory: {
+    textAlign: "center",
+    fontSize: "20px",
+    fontFamily: "Play, sans-serif",
+  },
+}));
